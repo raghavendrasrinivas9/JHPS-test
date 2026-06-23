c49ef37f5f44671db28a877cf86db4caa81a6786
raghavendrasrinivas9 <srinu.hvs@gmail.com>
2026-06-23 21:52:06 +0530

    Add files via upload

--- scripts/library.js
+++ scripts/library.js
@@ -38,9 +38,9 @@ window.libraryData = {
         title: "Mani Manjari",
         steps: [
             { id: 1, name: "Sarga-1", desc: "Historical account of the lineage and background.", audio: "#", video: "https://www.youtube.com/playlist?list=PLmggQwwEs0GilaFcOtPYFo-s063DP6QYg", pdfSanskrit: "downloads/mm-s1-sanskrit.pdf", pdfTelugu: "#", pdfKannada: "downloads/mm-s1-kannada.pdf" },
-			{ id: 2, name: "Sarga-2", desc: "Historical account of the lineage and background.", audio: "#", video: "https://youtube.com/playlist?list=PLmggQwwEs0GgryAaUSVQ22sZcY_VZ-azu&si=-509MatsmAjLUTXX", pdfSanskrit: "#", pdfTelugu: "#", pdfKannada: "downloads/mm-s2-kannada.pdf" },
-			{ id: 3, name: "Sarga-3", desc: "Historical account of the lineage and background.", audio: "#", video: "#", pdfSanskrit: "#", pdfTelugu: "#", pdfKannada: "downloads/mm-s3-kannada.pdf" },
-			{ id: 4, name: "Sarga-4", desc: "Historical account of the lineage and background.", audio: "#", video: "#", pdfSanskrit: "#", pdfTelugu: "#", pdfKannada: "#" }
+			{ id: 2, name: "Sarga-2", desc: "Historical account of the lineage and background.", audio: "#", video: "https://youtube.com/playlist?list=PLmggQwwEs0GgryAaUSVQ22sZcY_VZ-azu&si=-509MatsmAjLUTXX", pdfSanskrit: "downloads/mm-s1-sanskrit.pdf", pdfTelugu: "#", pdfKannada: "downloads/mm-s2-kannada.pdf" },
+			{ id: 3, name: "Sarga-3", desc: "Historical account of the lineage and background.", audio: "#", video: "#", pdfSanskrit: "downloads/mm-s1-sanskrit.pdf", pdfTelugu: "#", pdfKannada: "downloads/mm-s3-kannada.pdf" },
+			{ id: 4, name: "Sarga-4", desc: "Historical account of the lineage and background.", audio: "#", video: "#", pdfSanskrit: "downloads/mm-s1-sanskrit.pdf", pdfTelugu: "#", pdfKannada: "#" }
            
         ]
     },
